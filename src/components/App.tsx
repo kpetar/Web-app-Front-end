f6a3b27c80367ecdca75eebd4f174fcba3b33c15
Petar Kovac <petark218@gmail.com>
2021-04-17 13:59:38 +0200

    Install required libraries

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,13 @@
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Container } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Home
-    </div>
+    <Container>
+      <FontAwesomeIcon icon={faHome}></FontAwesomeIcon>Home
+    </Container>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from '../src/components/App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery';
+import 'popper.js/dist/popper';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import '@fortawesome/fontawesome-free/css/fontawesome.min.css';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
