797cfc4cdf4a9e72c3a34920d6670d8773759f00
Petar Kovac <petark218@gmail.com>
2021-05-07 13:33:45 +0200

    Implement the user logout mechanism

---
+++ src/components/UserLogoutPage/UserLogoutPage.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Redirect } from "react-router";
+import { removeTokenData } from "../../api/api";
+
+interface UserLogoutPageState
+{
+    //da li je komponenta zavrsila odjavu
+    done:boolean;
+}
+
+export class UserLogoutPage extends React.Component
+{
+    state:UserLogoutPageState;
+
+    constructor(props: {} | Readonly<{}>)
+    {
+        super(props);
+
+        this.state={
+            done:false,
+        }
+    }
+
+    private finished()
+    {
+        this.setState({done:true});
+    }
+
+    render()
+    {
+        if(this.state.done)
+        {
+            return (
+                <Redirect to="/user/login/"/>
+            );
+        }
+        return (
+            <p>Logging out...</p>
+        );
+    }
+
+    //kada se komponenta montira i update-uje
+    componentDidMount()
+    {
+        this.doLogout();
+    }
+
+    componentDidUpdate()
+    {
+        this.doLogout();
+    }
+
+    doLogout(){
+        removeTokenData("user");
+        this.finished();
+    }
+}
